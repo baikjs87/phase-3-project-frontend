e080f9c6d0c538277c449776bf625e895f9ffe0a
Jason Baik <baikjs87@gmail.com>
2022-08-12 01:56:11 -0400

    Working on CRUD

--- src/components/AddData.js
+++ src/components/AddData.js
@@ -5,16 +5,20 @@ function AddData({data}){
     const [city, setCity] = useState("")
     const [thingsToDo, setThingsToDo] = useState("")
     const [placesToGo, setPlacesToGo] = useState("")
-    const [name, setName] = useState("")
+
+    function handleCitySubmit(e) {
+        e.preventDefault()
+        console.log(city)
+    }
 
     return(
         <div className="form-container card">
             <div class="card-body">
                 <div className="new-interest-point">
                     <h5>Add Your New Interest Point</h5>
-                    <form className="city-form">
+                    <form className="city-form" onSubmit={handleCitySubmit}>
                         <label className="">
-                            <input type="text" value={city} placeholder="New City" onChange={(e) => setName(e.target.value)} className="city-input" />
+                            <input type="text" value={city} placeholder="New City" onChange={(e) => setCity(e.target.value)} className="city-input" />
                         </label>
                         <button type="submit" className="btn">Add</button>
                     </form>
@@ -24,7 +28,7 @@ function AddData({data}){
                     {/* <h6>New Things To Do</h6> */}
                     <form className="city-form">
                         <label className="">
-                            <input type="text" value={thingsToDo} placeholder="New Things To Do" onChange={(e) => setName(e.target.value)} className="city-input" />
+                            <input type="text" value={thingsToDo} placeholder="New Things To Do" onChange={(e) => setThingsToDo(e.target.value)} className="city-input" />
                             <select className="select city-input">
                                 <option selected>Select City</option>
                                 {data.map((data) => 
@@ -39,7 +43,7 @@ function AddData({data}){
                     {/* <h6>New Places To Go</h6> */}
                     <form className="city-form">
                         <label className="">
-                            <input type="text" value={placesToGo} placeholder="New Places To Go" onChange={(e) => setName(e.target.value)} className="city-input" />
+                            <input type="text" value={placesToGo} placeholder="New Places To Go" onChange={(e) => setPlacesToGo(e.target.value)} className="city-input" />
                             <select className="select city-input">
                                 <option selected>Select City</option>
                                 {data.map((data) => 
